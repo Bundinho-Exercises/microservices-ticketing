a9a679bf86fb78e8b7a760158f2071cf0c8d4874
Toky Mahefa Ralala <toky@wau.co>
2021-01-02 17:47:58 +0300

    enforce event subject check

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,17 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { Subjects } from "./subjects";
+import { TicketCreatedEvent } from "./ticket-created-event";
 
-export class TickedCreatedListener extends Listener {
-    subject = 'ticket:created';
+export class TickedCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
     onMessage(data: any, msg: Message): void {
         console.log('Event data!', data);
+
+        console.log(data.name);
+        console.log(data.price);
 
         msg.ack();
     }
